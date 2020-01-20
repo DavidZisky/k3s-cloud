50690d6ee08b49e7b16b16cff00fd7ce2ff1a787
Dawid Ziolkowski <david@zisky.net>
2020-01-20 14:41:10 +0100

    DO v1.0

---
+++ digitalocean/components/init.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+set -a
+source .env
+set +a
+
+if [[ -z ${do_api_token} ]] || [[ -z ${ssh_fingerprint} ]];
+then
+    echo "Token and/or SSH fingerprint values missing, please update .env file"
+    exit 1
+fi
+
+if grep -q "PUT_SSH_FINGERPRINT_HERE" components/droplets_workers.json; then
+  sed -i.bak "s/PUT_SSH_FINGERPRINT_HERE/$ssh_fingerprint/" components/droplets_workers.json
+fi
+
+if grep -q "PUT_SSH_FINGERPRINT_HERE" components/droplet_master.json; then
+  sed -i.bak "s/PUT_SSH_FINGERPRINT_HERE/$ssh_fingerprint/" components/droplet_master.json
+fi
+
+if grep -q "PUT_DO_TOKEN_HERE" delete_k3s_droplets.sh; then
+  sed -i.bak "s/PUT_DO_TOKEN_HERE/$do_api_token/" delete_k3s_droplets.sh
+fi
